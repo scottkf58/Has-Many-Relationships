cc01d210581016c577a440549dcaa3e487b7d3a7
Scott Fujimoto <scottfujimoto@Scotts-MacBook-Pro.local>
2017-07-27 15:13:25 -1000

    working on 9

--- joins.sql
+++ joins.sql
@@ -7,31 +7,37 @@
 
 -- -- 3
 -- SELECT posts.*, users.first_name, users.last_name
--- FROM posts
--- INNER JOIN users ON posts.users_id = users.id
+-- FROM posts INNER JOIN users
+-- ON posts.users_id = users.id
 -- WHERE users_id = 200;
 
 -- -- 4
 -- SELECT posts.*, users.username
--- FROM posts
--- INNER JOIN users ON posts.users_id = users.id
+-- FROM posts INNER JOIN users
+-- ON posts.users_id = users.id
 -- WHERE users.first_name = 'Norene' AND users.last_name = 'Schmitt';
 
 -- -- 5
 -- SELECT username
--- FROM users
--- INNER JOIN posts ON posts.users_id = users.id
+-- FROM users INNER JOIN posts
+-- ON posts.users_id = users.id
 -- WHERE posts.created_at > '2015-01-01';
 
 -- -- 6
 -- SELECT posts.title, posts.content, users.username
--- FROM users
--- INNER JOIN posts ON posts.users_id = users.id
+-- FROM users INNER JOIN posts
+-- ON posts.users_id = users.id
 -- WHERE users.created_at < '2015-01-01'
 
 -- -- 7
 -- SELECT comments.*, posts.title AS post_title
--- FROM posts
--- INNER JOIN comments ON comments.posts_id = posts.id;
+-- FROM posts INNER JOIN comments
+-- ON comments.posts_id = posts.id;
 
---8
+-- --8
+-- SELECT comments.*, posts.title AS post_title, posts.url AS post_url, comments.body AS comment_body
+-- FROM posts INNER JOIN comments
+-- ON comments.posts_id = posts.id
+-- WHERE posts.created_at < '2015-01-01';
+
+-- 9
