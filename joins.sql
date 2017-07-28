2ad434985cb42d50eea1b412107be5aa09cff0fb
Scott Fujimoto <scottfujimoto@Scotts-MacBook-Pro.local>
2017-07-27 14:44:05 -1000

    working on 7

--- joins.sql
+++ joins.sql
@@ -1,9 +1,37 @@
+-- -- 1
 -- SELECT * FROM users;
 
+-- -- 2
 -- SELECT * FROM posts
 -- WHERE users_id = 100;
 
-SELECT posts.*, users.first_name, users.last_name
-FROM posts
-INNER JOIN users ON posts.users_id = users.id
-WHERE users_id = 200;
+-- -- 3
+-- SELECT posts.*, users.first_name, users.last_name
+-- FROM posts
+-- INNER JOIN users ON posts.users_id = users.id
+-- WHERE users_id = 200;
+
+-- -- 4
+-- SELECT posts.*, users.username
+-- FROM posts
+-- INNER JOIN users ON posts.users_id = users.id
+-- WHERE users.first_name = 'Norene' AND users.last_name = 'Schmitt';
+
+-- -- 5
+-- SELECT username
+-- FROM users
+-- INNER JOIN posts ON posts.users_id = users.id
+-- WHERE posts.created_at > '2015-01-01';
+
+-- -- 6
+-- SELECT posts.title, posts.content, users.username
+-- FROM users
+-- INNER JOIN posts ON posts.users_id = users.id
+-- WHERE users.created_at < '2015-01-01'
+
+-- -- 7
+-- SELECT comments.*, posts.title AS post_title
+-- FROM posts
+-- INNER JOIN comments ON comments.posts_id = posts.id;
+
+--8
